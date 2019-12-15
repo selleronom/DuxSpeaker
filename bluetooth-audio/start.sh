d4f5f287621f28ea667f744eb8662f99b2501637
Erik <3093210+selleronom@users.noreply.github.com>
2019-12-15 22:18:16 +0100

    fix

--- bluetooth-audio/start.sh
+++ bluetooth-audio/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -z "$DEVICE_NAME" ]]; then
-  BLUETOOTH_DEVICE_NAME=$(printf "balenaSound %s" $(hostname | cut -c -4))
+  DEVICE_NAME=$(printf "balenaSound %s" $(hostname | cut -c -4))
 fi
 
 # Set the system volume here
